55e92d9912d1736f8903040a0dd289d77dce3c57
Dekana <chinenyeudechukwuu@gmail.com>
2022-06-25 07:35:00 +0100

    General setup

---
+++ emotion_game/game/urls.py
@@ -0,0 +1,12 @@
+# from django.urls import path
+#
+# from . import views
+#
+# app_name = 'game'
+#
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('', views.predictor, name='predictor'),
+#
+# ]
+
