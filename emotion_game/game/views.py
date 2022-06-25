b13cc01255033816ecf533baded8a9ef05c51851
Dekana <chinenyeudechukwuu@gmail.com>
2022-06-25 22:55:51 +0100

    trained and pickled model

--- emotion_game/game/views.py
+++ emotion_game/game/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+import math
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+from model.model import create_feature
+import pickle
 # Create your views here.
+
+model = pickle.load(open('model.pkl', 'rb+'))
+vectorizer = DictVectorizer(sparse=True)
+
+
+def home(request):
+
+    return render(request, 'home.html')
+
+
+def predictor(request):
+    if request.method == 'POST':
+        temp = dict()
+
+        user_entry = request.POST.get('user_entry')
+
+        features = create_feature(user_entry, nrange=(1, 4))
+        features = vectorizer.transform(features)
+
+        predicted_emotion = model.predict(user_entry)[0]
+
+        return render(request, 'result.html', {'result': predicted_emotion})
+
